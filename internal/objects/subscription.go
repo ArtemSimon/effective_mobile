0095fd1c60c8628cb192586a56eee6fb9547e97e
artem <artemsim2006@mail.ru>
2025-07-21 12:13:41 +0300

    add new struct for create and update request with json tags

--- internal/objects/subscription.go
+++ internal/objects/subscription.go
@@ -7,14 +7,31 @@ import (
 	"gorm.io/gorm"
 )
 
+// SubscriptionUpdateRequest определяет поля для обновления подписки
+
+type SubscriptionUpdateRequest struct {
+	ServiceName *string `json:"service_name,omitempty" example:"Netflix"`
+	Price       *int    `json:"price,omitempty" example:"599"`
+	EndDate     *string `json:"end_date,omitempty" example:"03-2025"`
+}
+
+// Отдельная структура для создания подписки
+type SubscriptionCreateRequest struct {
+	ServiceName string  `json:"service_name" example:"Netflix" binding:"required"`
+	Price       int     `json:"price" example:"599" binding:"required"`
+	UserID      string  `json:"user_id" example:"550e8400-e29b-41d4-a716-446655440000" binding:"required"`
+	StartDate   string  `json:"start_date" example:"09-2025" binding:"required"`
+	EndDate     *string `json:"end_date" example:"03-2025"`
+}
+
 // Основная структура системы
 type Subscription struct {
-	ID          uuid.UUID  `gorm:"type:uuid;primaryKey"`     // уникальный идендификатор
-	ServiceName string     `gorm:"not null"`                 // Название сервиса
-	Price       int        `gorm:"not null;check:price > 0"` // Цена подписки
-	UserID      uuid.UUID  `gorm:"type:uuid;not null"`       // уникальный id пользователя
-	StartDate   time.Time  `gorm:"not null"`                 // Начало активации подписки
-	EndDate     *time.Time // Окончание подписки
+	ID          uuid.UUID  `gorm:"type:uuid;primaryKey"  json:"id" example:"550e8400-e29b-41d4-a716-446655440000"`   // уникальный идендификатор
+	ServiceName string     `gorm:"not null" json:"service_name" example:"Netflix"`                                   // Название сервиса
+	Price       int        `gorm:"not null;check:price > 0" json:"price" example:"599"`                              // Цена подписки
+	UserID      uuid.UUID  `gorm:"type:uuid;not null" json:"user_id" example:"550e8400-e29b-41d4-a716-446655440000"` // уникальный id пользователя
+	StartDate   time.Time  `gorm:"not null" json:"start_date" swaggertype:"string" example:"09-2025"`                // Начало активации подписки
+	EndDate     *time.Time `json:"end_date,omitempty" swaggertype:"string" example:"03-2025"`                        // Окончание подписки
 }
 
 func (s *Subscription) IsActive(time_subscription time.Time) bool {
